f48783d1001b2d01513f9f61499b6c9ee0e2d01e
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2022-09-16 23:14:43 +0530

    added img To png

--- src/image/Image2Image.py
+++ src/image/Image2Image.py
@@ -1,9 +1,15 @@
+from PIL import Image
+
 class Image2Image:
     def __init__(self, path):
         self.path = path
+        self.extension = path.split('.')[-1]
     
-    def jpg2jpeg():
-        pass
+    def ToPng(self):
+        image = Image.open(self.path);
+        rgb_image = image.convert('RGB');
+        rgb_image.save(f"{self.path.split('.')[0]}.png") # save image as png
 
-    def jpeg2jpg():
-        pass
+if __name__ == "__main__":
+    Image2Image("test\pic.jpg").ToPng()
+
