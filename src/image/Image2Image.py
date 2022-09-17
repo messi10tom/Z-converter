18f01344431fc7a92d3a46ce5c87dcb6d7f09726
Messi Tom <91322610+messi10tom@users.noreply.github.com>
2022-09-17 22:44:53 +0530

    Update Image2Image.py

--- src/image/Image2Image.py
+++ src/image/Image2Image.py
@@ -1,15 +1,22 @@
 from PIL import Image
 
 class Image2Image:
-    def __init__(self, path):
+    def __init__(self, path, name):
         self.path = path
+        self.name = name
         self.format = path.split('.')[-1].lower()
         self.image = Image.open(self.path);
         self.rgb_image = self.image.convert('RGB');
     
     def ToPng(self):
         return self.rgb_image.save(f"{self.path.split('.')[0]}.png")
+    
+    def jpg2jpeg(self):
+        os.rename(self.path, os.path.join(os.path.dirname(self.path), self.name + ".jpeg"))
 
+    def jpeg2jpg(self):
+        os.rename(self.path, os.path.join(os.path.dirname(self.path), self.name + ".jpg"))
+        
     def ToJpg(self):
         return self.rgb_image.save(f"{self.path.split('.')[0]}.jpg")
 
